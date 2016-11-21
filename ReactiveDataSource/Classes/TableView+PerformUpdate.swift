6b3f675d3117818a72750c5b2dbc8da161c2f117
Tulipa <darwin.jxzang@gmail.com>
2016-11-21 15:35:32 +0800

    tmp

--- ReactiveDataSource/Classes/TableView+PerformUpdate.swift
+++ ReactiveDataSource/Classes/TableView+PerformUpdate.swift
@@ -9,7 +9,12 @@
 import Foundation
 
 extension UITableView: ReactiveDataSourceDelegate {
+    
     public func perform(_ operation: ChangeOperation) {
+        perform(operation, action: .automatic)
+    }
+    
+    public func perform(_ operation: ChangeOperation, action: UITableViewRowAnimation) {
         beginUpdates()
         operation.commit()
         operation.changes.forEach({ (change) in
@@ -17,23 +22,23 @@ extension UITableView: ReactiveDataSourceDelegate {
             case .add(let add):
                 switch add {
                 case .section(let section):
-                    insertSections(IndexSet(integer: section), with: .automatic)
+                    insertSections(IndexSet(integer: section), with: action)
                 case .item(let item):
-                    insertRows(at: [item], with: .automatic)
+                    insertRows(at: [item], with: action)
                 }
             case .remove(let remove):
                 switch remove {
                 case .section(let section):
-                    deleteSections(IndexSet(integer: section), with: .automatic)
+                    deleteSections(IndexSet(integer: section), with: action)
                 case .item(let item):
-                    deleteRows(at: [item], with: .automatic)
+                    deleteRows(at: [item], with: action)
                 }
             case .reload(let reload):
                 switch reload {
                 case .item(let item):
-                    reloadRows(at: [item], with: .automatic)
+                    reloadRows(at: [item], with: action)
                 case .section(let section):
-                    reloadSections(IndexSet(integer: section), with: .automatic)
+                    reloadSections(IndexSet(integer: section), with: action)
                 }
             }
         })
